28efb59c96e761e5c2e3b3ae591c97f57eaafec3
azbagas <azharbagaskara1@gmail.com>
2025-02-15 10:44:44 +0700

    feat: new.sh script to auto create topic folder

--- new.sh
+++ new.sh
@@ -6,8 +6,16 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Enter the solution name (topic/00-problem-name.py):${NC}"
 read name
 
-command=`cp template.py $name`
+# Extract the topic folder from the input
+topic_folder=$(dirname "$name")
 
-echo $command
+# Check if the topic folder exists, if not, create it
+if [ ! -d "$topic_folder" ]; then
+    mkdir -p "$topic_folder"
+    echo -e "${GREEN}Created folder: $topic_folder${NC}"
+fi
 
-echo -e "${GREEN}File created successfully. Happy solving!"
+# Copy the template to the new file
+cp template.py "$name"
+
+echo -e "${GREEN}File created successfully at $name. \nHappy solving!${NC}"
